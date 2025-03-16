42581876ab9c1e75a672f4a2fb9b001ccbf251b1
Mike Jackson <mike@airtightdesign.com>
2025-03-15 22:00:33 -0400

    BMR calculation

--- src/components/Calculations.jsx
+++ src/components/Calculations.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 const calculateResults = ( data ) => {
     const {age, sex, height, weight, activity} = data;
-    console.log(data);
+    let BMR = 0;
+    if (data.sex === "male") {
+        BMR = 10 * data.weight + 6.25 * data.height - 5 * data.age + 5;
+    } else if (data.sex === "female") {
+        BMR = 10 * data.weight + 6.25 * data.height - 5 *data. age - 161;
+    }
+    
+    console.log(BMR);
 }
 
 export default calculateResults;
